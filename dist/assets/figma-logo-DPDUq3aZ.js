884098b8311a2dec93c94acc4183c3d16eeeea84
Dirga Febriyanda <dirgafebriyanda12@gmail.com>
2024-11-30 20:00:41 +0700

    first commit

---
+++ dist/assets/figma-logo-DPDUq3aZ.js
@@ -0,0 +1,1 @@
+const o="/vue-js/images/photoshop-logo.png",s="/vue-js/images/corel-logo.png",g="/vue-js/images/illustrator-logo.png",t="/vue-js/images/figma-logo.png";export{o as _,s as a,g as b,t as c};
